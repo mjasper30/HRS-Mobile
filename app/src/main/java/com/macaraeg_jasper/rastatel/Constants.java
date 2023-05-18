571970023616151b81b6007f3dd235c6386b90e6
mjasper30 <jasper.macaraeg42@gmail.com>
2023-05-18 21:17:46 +0800

    login and register

---
+++ app/src/main/java/com/macaraeg_jasper/rastatel/Constants.java
@@ -0,0 +1,9 @@
+package com.macaraeg_jasper.rastatel;
+
+public class Constants {
+    //192.168.68.105
+    private static final String ROOT_URL = "http://192.168.68.105/HRS_FINAL/admin/v1/";
+
+    public static final String URL_REGISTER = ROOT_URL+"registerUser.php";
+    public static final String URL_LOGIN = ROOT_URL+"userLogin.php";
+}
